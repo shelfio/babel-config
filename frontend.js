b7a142cf93b19223617a2dc1adf750150d7fd355
Khmel Mykyta <hmelenok@gmail.com>
2021-03-11 11:41:02 +0200

    feat: main frontend configs

---
+++ frontend.js
@@ -0,0 +1,26 @@
+/**
+ * React components without browser boilerplate (jsx,tsx) + testing (ts/tsx)
+ */
+module.exports = {
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    'babel-plugin-styled-components',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: false,
+        regenerator: true,
+        useESModules: true,
+        absoluteRuntime: false,
+      },
+    ],
+  ],
+  presets: ['@babel/preset-typescript', '@babel/preset-env', '@babel/preset-react'],
+  env: {
+    test: {
+      presets: ['@babel/preset-typescript', '@babel/preset-env'],
+    },
+  },
+};
